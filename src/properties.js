ee1d042e39148076cb50bdee8946e0d743ea5243
Arturo <arturovelazquez94@gmail.com>
2022-12-13 11:00:11 -0600

    Change host myasin-api.myasinmanager.org

--- src/properties.js
+++ src/properties.js
@@ -1,4 +1,4 @@
 export const properties = {
     // HOST: "http://ec2-3-231-59-47.compute-1.amazonaws.com:8080" ->EC2
-    HOST: "https://api.myasinmanager.org" // -> ELB 
+    HOST: "https://myasin-api.myasinmanager.org" // -> ELB 
 };
