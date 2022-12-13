48f5a0600bbb6e4efc3811aac7f6097241572d22
Arturo <arturovelazquez94@gmail.com>
2022-12-13 09:08:29 -0600

    Change to load balancer host

--- src/properties.js
+++ src/properties.js
@@ -1,3 +1,4 @@
 export const properties = {
-    HOST: "https://ec2-3-231-59-47.compute-1.amazonaws.com:8080"
+    // HOST: "http://ec2-3-231-59-47.compute-1.amazonaws.com:8080" ->EC2
+    HOST: "https://MYASINMANAGER-LB-1547822898.us-east-1.elb.amazonaws.com:443" // -> ELB 
 };
