5d67829e4b0ad4535862ffcd011949d0ca2cb71e
Arturo <arturovelazquez94@gmail.com>
2022-12-13 10:06:19 -0600

    Blank chanhge

--- src/properties.js
+++ src/properties.js
@@ -2,3 +2,4 @@ export const properties = {
     // HOST: "http://ec2-3-231-59-47.compute-1.amazonaws.com:8080" ->EC2
     HOST: "https://MYASINMANAGER-LB-1547822898.us-east-1.elb.amazonaws.com:443" // -> ELB 
 };
+
