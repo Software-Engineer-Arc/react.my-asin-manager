541f7bf317836963651ee5b172dd201ad3e97a71
Arturo <arturovelazquez94@gmail.com>
2022-12-13 19:48:22 -0600

    Add localhost as domain

--- src/properties.js
+++ src/properties.js
@@ -1,4 +1,5 @@
 export const properties = {
     // HOST: "http://ec2-3-231-59-47.compute-1.amazonaws.com:8080" ->EC2
     HOST: "https://api.myasinmanager.org" // -> ELB 
+   // HOST: "http://localhost:8080" // -> Localhost 
 };
